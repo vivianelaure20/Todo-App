9a2f94be32c75ecd1e45fbfb7fcba1a6ca601bf3
Vaibhav Lone <lone.vaibhav7@gmail.com>
2020-03-31 16:59:08 +0200

    This

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,68 +1,50 @@
+import React, { useState } from "react";
 
-import TodoItems from './TodoItems.js';
-import './TodoList.css';
-import React, {useState} from 'react';
+import TodoItems from "./TodoItems.js";
+import "./TodoList.css";
 
-class TodoList extends useState{
-constructor(props){
-  super(props);
+const TodoList = ({}) => {
+  const [items, setItems] = useState([]);
 
-  this.state ={
-    items:[]
+  const addItem = e => {
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+
+      this.setState(prevState => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+    }
+    this._inputElement.value = "";
+    console.log(this.state.items);
+    e.preventDefault();
   };
 
-  this.addItem = this.addItem.bind(this);
-  this.deleteItem = this.deleteItem.bind(this);
-
-}
-
-addItem(e){
-  if(this._inputElement.value!==""){
-    var newItem ={
-      text: this._inputElement.value,
-      key: Date.now()
-    };
-
-    this.setState((prevState)=> {
-      return{
-        items:prevState.items.concat(newItem)
-      };
+  const deleteItem = key => {
+    var filteredItems = this.state.items.filter(function(item) {
+      return item.key !== key;
     });
-  }
-this._inputElement.value="";
-console.log(this.state.items);
-e.preventDefault();
-}
 
-deleteItem(key){
-  console.log("Key in deleteItem:" +key);
-  console.log("Items at delete:" +this.state.items);
-  var filteredItems=this.state.items.filter(function (item){
-return (item.key !==key)
+    this.setState({
+      items: filteredItems
+    });
+  };
 
-  });
-
-  this.setState({
-    
-    items:filteredItems
-  });
-
-}
-
-render(){
-  return(
+  return (
     <div className="todoListMain">
-    <div className="header">
-    <form onSubmit={this.addItem}>
-      <input ref={(a)=>this._inputElement=a} placeholder="Enter Task"></input>
-      <button type="submit">add</button>
-    </form>
-    </div>
-    <TodoItems entries ={this.state.items} 
-         delete={this.deleteItem}/>
+      <div className="header">
+        <form onSubmit={this.addItem}>
+          <input placeholder="Enter Task"></input>
+          <button type="submit">add</button>
+        </form>
+      </div>
+      <TodoItems entries={this.state.items} delete={this.deleteItem} />
     </div>
   );
-}
-}
+};
 
 export default TodoList;
