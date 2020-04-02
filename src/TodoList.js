01445f675ff6df270fb9995eac8d6a5737efa826
Chad Faurie <54393506+chad-codeworkshop@users.noreply.github.com>
2020-04-02 09:49:53 +0200

    Notes on how to fix TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,26 +4,37 @@ import "./TodoList.css";
 
 const TodoList = ({}) => {
   const [items, setItems] = useState([]);
+  // use this in the on change of input
+  // also use text in the value of input
+  // const [text, setText] = useState("");
 
   const addItem = e => {
+    // this. does not exist, neither does _inputElement
     if (this._inputElement.value !== "") {
       var newItem = {
         text: this._inputElement.value,
         key: Date.now()
       };
 
+      // this. and setState do not exist, use a useState var
       this.setState(prevState => {
         return {
           items: prevState.items.concat(newItem)
         };
       });
     }
+    
+    
     this._inputElement.value = "";
     console.log(this.state.items);
     e.preventDefault();
+    
+    // reset text state here
   };
 
-  const deleteIems = key => {
+  // TodoItems duplicates the delete function, you should use this one and not the one in the other component
+  // Extract it like this ({deleteItem})
+  const deleteItem = key => {
     var filteredItems = this.state.items.filter(function(item) {
       return item.key !== key;
     });
@@ -38,6 +49,7 @@ const TodoList = ({}) => {
       <div className="header">
         <form onSubmit={setItems.addItem}>
           <input
+            // This should not be here, set it to another state variable
             onChange={e => setItems(e.target.value)}
             placeholder="Enter Task"
           ></input>
@@ -46,7 +58,8 @@ const TodoList = ({}) => {
           </button>
         </form>
       </div>
-      <TodoItems entries={setItems.newItem} deleteItem={setItems.deleteIems} />
+      // Spelling Fix 
+      <TodoItems entries={setItems.newItem} deleteItem={setItems.deleteItem} />
     </div>
   );
 };
